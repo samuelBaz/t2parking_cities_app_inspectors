22ce6596bd0dcd2c2c8d9b0bb49e8b45288c7c28
Samuel Bazoalto <sbazoalto@t2company.com.uy>
2025-05-09 13:25:52 -0400

    minos changes

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,24 +1,14 @@
 import 'package:intl/intl.dart';
 
 String formatUtcToLocal(String utcDateString) {
-  // Parsear la fecha en formato UTC
   DateTime utcDate = DateTime.parse(utcDateString);
-
-  // Convertir a la zona horaria local
-  // tz.TZDateTime localDate = tz.TZDateTime.from(utcDate, tz.local);
-
-  // Formatear la fecha en un formato legible
-  String formattedDate = DateFormat('dd/MM/yyyy HH:mm').format(utcDate);
+  String formattedDate = DateFormat('dd/MM/yyyy HH:mm').format(utcDate.toLocal());
   return formattedDate;
 }
 
 String formatUtcTo3Letters(String utcDateString) {
-
-  // Parsear la fecha en formato UTC
   DateTime utcDate = DateTime.parse(utcDateString);
-
-  // Formatear la fecha en un formato legible
-  String formattedDate = DateFormat('dd MMM, yyyy').format(utcDate);
+  String formattedDate = DateFormat('dd MMM, yyyy').format(utcDate.toLocal());
   formattedDate = formattedDate.replaceAll('Jun', 'Ene');
   formattedDate = formattedDate.replaceAll('Apr', 'Abr');
   formattedDate = formattedDate.replaceAll('Aug', 'Ago');
@@ -27,10 +17,7 @@ String formatUtcTo3Letters(String utcDateString) {
 }
 
 String formatUtcToHours(String utcDateString) {
-  // Parsear la fecha en formato UTC
   DateTime utcDate = DateTime.parse(utcDateString);
-
-  // Formatear la fecha en un formato legible
-  String formattedDate = DateFormat('HH:mm').format(utcDate);
+  String formattedDate = DateFormat('HH:mm').format(utcDate.toLocal());
   return formattedDate;
 }
